23b7cfdcbb45115ac696f3448d3238d74499b23a
melck <melck_junior@hotmail.com>
2022-08-12 14:55:39 +0100

    Section

--- backend/src/main/java/com/mck/mcklearn/entities/Section.java
+++ backend/src/main/java/com/mck/mcklearn/entities/Section.java
@@ -1,16 +1,12 @@
 package com.mck.mcklearn.entities;
 
-import com.mck.mcklearn.entities.enums.ResourceType;
-
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_resource")
-public class Resource implements Serializable {
+@Table(name = "tb_section")
+public class Section implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,26 +15,28 @@ public class Resource implements Serializable {
     private String description;
     private Integer position;
     private String imgUri;
-    private ResourceType type;
+
 
     @ManyToOne
-    @JoinColumn(name = "offer_id")
-    private Offer offer;
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
 
-    @OneToMany(mappedBy = "resource")
-    private List<Section> sections = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "prerequisite_id")
+    private Section prerequisite;
 
-    public Resource() {
+
+    public Section() {
     }
 
-    public Resource(long id, String title, String description, Integer position, String imgUri, ResourceType type, Offer offer) {
+    public Section(long id, String title, String description, Integer position, String imgUri, Resource resource, Section prerequisite) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.position = position;
         this.imgUri = imgUri;
-        this.type = type;
-        this.offer = offer;
+        this.resource = resource;
+        this.prerequisite = prerequisite;
     }
 
     public long getId() {
@@ -81,28 +79,28 @@ public class Resource implements Serializable {
         this.imgUri = imgUri;
     }
 
-    public ResourceType getType() {
-        return type;
+    public Resource getResource() {
+        return resource;
     }
 
-    public void setType(ResourceType type) {
-        this.type = type;
+    public void setResource(Resource resource) {
+        this.resource = resource;
     }
 
-    public Offer getOffer() {
-        return offer;
+    public Section getPrerequisite() {
+        return prerequisite;
     }
 
-    public void setOffer(Offer offer) {
-        this.offer = offer;
+    public void setPrerequisite(Section prerequisite) {
+        this.prerequisite = prerequisite;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Resource)) return false;
-        Resource resource = (Resource) o;
-        return getId() == resource.getId();
+        if (!(o instanceof Section)) return false;
+        Section Section = (Section) o;
+        return getId() == Section.getId();
     }
 
     @Override
@@ -110,3 +108,4 @@ public class Resource implements Serializable {
         return Objects.hash(getId());
     }
 }
+
