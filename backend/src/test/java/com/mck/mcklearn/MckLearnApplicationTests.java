e149e64ba7e1264dcef07bfc4feec5b8b46d6a96
melck <melck_junior@hotmail.com>
2022-08-12 11:31:22 +0100

    Backend project created

---
+++ backend/src/test/java/com/mck/mcklearn/MckLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mck.mcklearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MckLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
